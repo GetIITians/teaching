e0cf49cbd596209407576c52e9516d0f80e4fa16
Yogy <ysaini75@gmail.com>
2015-11-04 13:04:49 +0530

    Hello

--- application/controllers/caller.php
+++ application/controllers/caller.php
@@ -6,8 +6,6 @@ class caller extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model("General_model","general");
-		$this->load->model("Caller_model","caller");
 		$this->tempname = "Template/CI_template"; 
 		$this->config->set_item('enable_query_strings', FALSE);
 		$this->load->helper('help_helper');
@@ -22,6 +20,8 @@ class caller extends CI_Controller
 
 	function view($id)
 	{	
+		$this->load->model("General_model","general");
+		$this->load->model("Caller_model","caller");
 		$data["view_name"] = "Caller/single_view";
 		$data["view_data"]["caller_info"] = $this->general->get_records("caller_details",array("id"=>$id))[0];
 		$data["view_data"]["teaching_info"] = $this->caller->teachinginfo($id)[0]; 
