6d49fd5517212c0689b8faa7c0b6c9421fb304bc
Yogy <ysaini75@gmail.com>
2015-11-04 13:34:44 +0530

    Hello

--- application/models/General_model.php
+++ application/models/General_model.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class General_model extends CI_Model
+class general_model extends CI_Model
 {
 		function add_record($table_name,$data) 
 	{
