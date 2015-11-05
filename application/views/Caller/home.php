2e8f8359e17c5765897740ec8593e55f66a0cfed
Yogy <ysaini75@gmail.com>
2015-11-05 14:23:55 +0530

    caller Work

--- application/views/Caller/home.php
+++ application/views/Caller/home.php
@@ -1,5 +1,6 @@
 <?php 
-$teacher_info = Sqle::getA("SELECT * from caller_teacher");
+$teacher_info = Sqle::getA("SELECT users.id,users.name from users INNER JOIN teachers ON teachers.tid = users.id where teachers.isselected = 'a' ");
+
 load_view("popup.php",array("name"=>"addcollerpopup", "title" => "New Entry Registration Form","body" =>"Caller/addcaller.php","bodyinfo" => array("teacher_info" => $teacher_info,"caller_details" => array("id"=>"","action"=>"callerinfo","popup_close"=>"addcollerpopup","name"=>"","email"=>"","phone"=>"","address"=>"","class"=>"","subject"=>"","board"=>"","tution_type"=>"","caller_name"=>"","caller_rel"=>"","created_at"=>"","updated_at"=>"","caller_date"=>"","comments"=>"")) )); 
 load_view("popup.php",array("name"=>"addteacherpopup", "title" => "Add A Teacher","body" =>"Caller/addteacher.php","bodyinfo" => array() )); 
 load_view("popup.php",array("name"=>"editcollerpopup", "title" => "Edit Details" )); 
@@ -12,9 +13,9 @@ load_view("popup.php",array("name"=>"editcollerpopup", "title" => "Edit Details"
   <div class="col-sm-2" style="margin-top:10px">
   <a href="#" class="btn">Send Message</a>
   </div>
-  <div class="col-sm-2" style="margin-top:10px">
+  <!-- <div class="col-sm-2" style="margin-top:10px">
   <a  class="btn" onclick='mohit.popup("addteacherpopup")'>Add A Teacher</a>
-  </div>
+  </div> -->
   </div>
   <div class="row" style="margin-top:10px" id="callertlb" data-action="callertbl">
   	<?php
