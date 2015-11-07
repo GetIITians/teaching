1cf4eb404139a37ab0ebba7688be0366936c979f
Yogy <ysaini75@gmail.com>
2015-11-07 21:06:42 +0530

    caller work

--- application/controllers/caller.php
+++ application/controllers/caller.php
@@ -9,20 +9,33 @@ class caller extends CI_Controller
 		$this->tempname = "Template/CI_template"; 
 		$this->config->set_item('enable_query_strings', FALSE);
 		$this->load->helper('help_helper');
+		$this->unauthenticatepage = "Template/unauthenticatepage";
 	}
 
 	function index()
 	{
-		$data["view_name"] = "Caller/home";
 		$data["view_data"] = "";
+		if(User::isloginas('a')){
+		$data["view_name"] = "Caller/home";
 		$this->load->view($this->tempname,$data);
+		}	
+		else{
+			$data["view_name"] = $this->unauthenticatepage; 
+			$this->load->view($this->tempname,$data);
+		}
 	}
 
 	function view($id)
-	{	
+	{	$data["view_data"]="";
+		if(User::isloginas('a')){
 		$data["view_name"] = "Caller/single_view";
 		$this->db->where(array("id"=>$id));
 		$data["view_data"]["caller_info"] =  $this->db->get("caller_details")->result_array()[0];
 		$this->load->view($this->tempname,$data);	
+		}	
+		else{
+			$data["view_name"] = $this->unauthenticatepage; 
+			$this->load->view($this->tempname,$data);
+		}
 	}
 }
---
+++ application/views/Template/unauthenticatepage.php
@@ -0,0 +1,46 @@
+<style type="text/css">
+	.mybody { background-color: rgba(0, 105, 92, 0.55);
+		border-radius: 10px
+	 }
+.myani {
+  padding: 40px;
+  font-size: 40px;
+  text-align: center;
+  color: red;
+  font-family: cursive;
+
+    -webkit-animation-name: example; /* Chrome, Safari, Opera */
+    -webkit-animation-duration: 100s; /* Chrome, Safari, Opera */
+    -webkit-animation-iteration-count: infinite; /* Chrome, Safari, Opera */
+    animation-name: example;
+    animation-duration: 100s;
+    animation-iteration-count: infinite;
+}
+
+/* Chrome, Safari, Opera */
+@-webkit-keyframes example {
+    0%   {color:red; left:0px; top:0px;}
+    25%  {color:yellow; left:200px; top:0px;}
+    50%  {color:blue; left:200px; top:200px;}
+    75%  {color:green; left:0px; top:200px;}
+    100% {color:red; left:0px; top:0px;}
+}
+
+/* Standard syntax */
+@keyframes example {
+    0%   {color:red; left:0px; top:0px;}
+    25%  {color:yellow; left:200px; top:0px;}
+    50%  {color:blue; left:200px; top:200px;}
+    75%  {color:green; left:0px; top:200px;}
+    100% {color:red; left:0px; top:0px;}
+}
+</style>
+
+</style>
+<div class="container mybody mt20">
+<div class="myani">
+  <p id="error">E<span>rr</span>or</p>
+  <p id="code">You<span> are not a</span><span> valid user</span></p>
+</div>
+
+</div>
