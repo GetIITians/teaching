b8a4626047ba7c8f59ecd42263674fefcbc101e9
Yogy <ysaini75@gmail.com>
2015-11-27 15:14:52 +0530

    caller next pervious

--- application/views/Caller/single_view.php
+++ application/views/Caller/single_view.php
@@ -2,6 +2,14 @@
 load_view("popup.php",array("name"=>"callerhistorypopup", "title" =>"Add ".$caller_info['name']."'s Caller Details","body" =>"Caller/calldetailsform.php","bodyinfo" => array("st_id"=>$caller_info['id']) )); 
 ?>
 <div class="container" >
+<div class="row mt5">
+	<div class="col-sm-1" >
+		<a class="btn btn-primary" href="<?php echo HOST.'caller/view/'.($caller_info['id']-1); ?>" >Pervious</a>
+	</div>
+	<div class="col-sm-1" style="float:right">	
+		<a class="btn btn-primary" href="<?php echo HOST.'caller/view/'.($caller_info['id']+1); ?>" >Next</a>
+	</div>
+</div>
 	<div data-action="caller_basicinfo" id="caller_basic_info" data-id=<?php echo $caller_info['id']; ?>>
 		<?php 
 	  		handle_disp(array("id"=>$caller_info['id']), "caller_basicinfo");
